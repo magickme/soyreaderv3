a190e487f4b0c3104dfef84120484749f758ff81
Jason Louv <jason@ultraculture.org>
2022-06-04 23:24:10 -0500

    Building out API test. Commiting prior to installing node-fetch

--- src/stores/api/api.test.js
+++ src/stores/api/api.test.js
@@ -17,11 +17,58 @@
 
 import { writable } from 'svelte/store';
 
-const sum = require('./api');
-
 describe('Reddit API call', () => {
-
-    it('adds 1+2 to equal 3', () => {
-        expect(sum(1, 2)).toBe(3);
+    describe('Define and set default value of variables', () => {
+        it('defines subreddit', () => {
+            let subreddit = 'all';
+        });
+        it('defines sort order', () => {
+            let sort = 'top';
+        });
+        it('defines timeframe', () => {
+            let time = 'today';
+        });
+        it('defines limit', () => {
+            let limit = 5;
+        });
+    });
+    describe('Make simple call to Reddit API', () => {
+        it('make aync call, no variables, test that it responds', async() => {
+            const url = 'https://www.reddit.com/r/all/top.json/?t=$today&limit=5';
+            const res = await fetch(url);
+            console.log(res);
+            expect(res).not.toBeNull();
+        })
     })
 });
+
+
+
+
+/*
+
+ it('Define URL', () => {
+            const url: string = `https://www.reddit.com/r/${sub}/${sort}.json?t=${time}&limit=${limit}`;
+        })
+    });
+
+export const subreddit = writable([]);
+
+export const fetchSubreddit = async(sub, sort, time, limit) => {
+    const url = `https://www.reddit.com/r/${sub}/${sort}.json?t=${time}&limit=${limit}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    const loadedSubreddit = data.data.children.map((data, index) => ({
+        id: index + 1,
+        title: data.data.title,
+        author: data.data.author,
+        thumbnail: data.data.thumbnail,
+        selftext: data.data.selftext,
+        subreddit_name_prefixed: data.data.subreddit_name_prefixed,
+        url: data.data.url
+    }));
+    subreddit.set(loadedSubreddit);
+};
+fetchSubreddit("linux", "top", "today", 100);
+
+*/
