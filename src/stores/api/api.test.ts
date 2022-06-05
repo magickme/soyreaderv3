32a2a194659888dc8251ba26d9ed6cfa9520151d
Jason Louv <jason@ultraculture.org>
2022-06-05 01:13:39 -0500

    Transpiled api.ts and imported into index.svelte

--- src/stores/api/api.test.ts
+++ src/stores/api/api.test.ts
@@ -23,8 +23,8 @@ describe('Reddit API call', () => {
         it('simple API call, no variables', async() => {
             const redditUrl: string = 'https://www.reddit.com/r/all/top.json?t=today&limit=5';
             const res = await fetch(redditUrl);
-            const redditData = await res.json();
-            const redditLoad = redditData.data.children.map((redditData: any, index: number) => ({
+            const redditData: any = await res.json();
+            const redditLoad: any = redditData.data.children.map((redditData: any, index: number) => ({
                 id: index + 1,
                 title: redditData.data.title,
                 subreddit_name_prefixed: redditData.data.subreddit_name_prefixed,
--- src/stores/api/api.ts
+++ src/stores/api/api.ts
@@ -1,3 +1,7 @@
+/**
+ * This should be called once per Feed component, and pass variables into redditUrl based on user input in order to display different subreddits and filter posts by date range, sorting method and post limit.
+ */
+
 require('isomorphic-fetch');
 import { writable } from 'svelte/store';
 export const fetchedSubreddit = writable([]);
@@ -5,8 +9,8 @@ export const fetchedSubreddit = writable([]);
 export const fetchSubreddit = async() => {
     const redditUrl: string = 'https://www.reddit.com/r/all/top.json?t=today&limit=5';
     const res = await fetch(redditUrl);
-    const redditData = await res.json();
-    const redditLoad = redditData.data.children.map((redditData: any, index: number) => ({
+    const redditData: any = await res.json();
+    const redditLoad: any = redditData.data.children.map((redditData: any, index: number) => ({
         id: index + 1,
         title: redditData.data.title,
         subreddit_name_prefixed: redditData.data.subreddit_name_prefixed,
